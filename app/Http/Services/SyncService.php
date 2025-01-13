9214c29783d1019381317af164cbe59683fb39c8
saeedmoradiuok <saeedmoradi.uok@gmail.com>
2025-01-13 06:17:55 +0330

    version number changed

--- app/Http/Services/SyncService.php
+++ app/Http/Services/SyncService.php
@@ -7,7 +7,7 @@ use App\Http\Responses\ApiResponse;
 class SyncService{
 
     public static function syncData(){
-        return ApiResponse::syncResponse(true,'data synced','3.0');
+        return ApiResponse::syncResponse(true,'data synced','4.0');
     }
 
 }
